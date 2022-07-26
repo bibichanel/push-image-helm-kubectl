53fc7db0c0c6e3c6ffa4e76e49a402298bd29edc
bibichanel <bibichanel@kali.bibichanel>
2022-07-26 09:16:39 -0400

    change files

--- push-image.sh
+++ push-image.sh
@@ -1,5 +1,5 @@
 kubectl config view --raw  > ./config
 docker build -t bibichanel/helm-kubectl:latest .
-docker login --username "bibichanel" --password "Maiyeuem123"
+docker login --username "$REGISTRY_USERNAME"  --password "$REGISTRY_PASSWORD"
 docker push bibichanel/helm-kubectl:latest
 docker rmi bibichanel/helm-kubectl:latest
